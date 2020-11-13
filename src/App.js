045df2a35c376532c843df6f842e176741b187c8
Jeanne Lima <23726423+JeanneLima@users.noreply.github.com>
2020-11-12 22:22:26 -0300

    Adiciona perguntas e respostas corretas

--- src/App.js
+++ src/App.js
@@ -3,39 +3,103 @@ import React, { useState } from 'react';
 export default function App() {
 	const questions = [
 		{
-			questionText: 'What is the capital of France?',
+			questionText: 'Dentre as opções, qual pasta do Pinterest eu não criei para nós?',
 			answerOptions: [
-				{ answerText: 'New York', isCorrect: false },
-				{ answerText: 'London', isCorrect: false },
-				{ answerText: 'Paris', isCorrect: true },
-				{ answerText: 'Dublin', isCorrect: false },
+				{ answerText: 'Amor', isCorrect: false },
+				{ answerText: 'Girafas', isCorrect: false },
+				{ answerText: 'Adoralina *-*', isCorrect: true },
+				{ answerText: 'D', isCorrect: false },
+				{ answerText: 'Stars, Galaxies & Love', isCorrect: false },
 			],
 		},
 		{
-			questionText: 'Who is CEO of Tesla?',
+			questionText: 'Qual estampa de roupa do mesmo estilo já usamos juntas?',
 			answerOptions: [
-				{ answerText: 'Jeff Bezos', isCorrect: false },
-				{ answerText: 'Elon Musk', isCorrect: true },
-				{ answerText: 'Bill Gates', isCorrect: false },
-				{ answerText: 'Tony Stark', isCorrect: false },
+				{ answerText: 'Florida', isCorrect: true },
+				{ answerText: 'Xadrez', isCorrect: false },
+				{ answerText: 'Degradê', isCorrect: false },
+				{ answerText: 'Bolinhas', isCorrect: false },
+				{ answerText: 'Cor sólida', isCorrect: false },
 			],
 		},
 		{
-			questionText: 'The iPhone was created by which company?',
+			questionText: 'Qual dos nomes abaixo você nunca teve no meu celular?',
 			answerOptions: [
-				{ answerText: 'Apple', isCorrect: true },
-				{ answerText: 'Intel', isCorrect: false },
+				{ answerText: 'Deborah Cardoso', isCorrect: false },
+				{ answerText: 'Adora', isCorrect: false },
+				{ answerText: 'Mon Lapin', isCorrect: false },
+				{ answerText: 'Deba', isCorrect: true },
+				{ answerText: 'Amora', isCorrect: false },
+			],
+		},
+		{
+			questionText: 'Em que ano começamos a nos falar?',
+			answerOptions: [
+				{ answerText: '2018', isCorrect: false },
+				{ answerText: '2015', isCorrect: false },
+				{ answerText: '2016', isCorrect: true },
+				{ answerText: '2019', isCorrect: false },
+				{ answerText: '2017', isCorrect: false },
+			],
+		},
+		{
+			questionText: 'De onde eu nunca comprei presente para você?',
+			answerOptions: [
+				{ answerText: 'Mercado Livre', isCorrect: false },
+				{ answerText: 'Livraria Família Cristã', isCorrect: true },
+				{ answerText: 'Foom', isCorrect: false },
 				{ answerText: 'Amazon', isCorrect: false },
-				{ answerText: 'Microsoft', isCorrect: false },
+				{ answerText: 'Livraria da travessa', isCorrect: false },
 			],
 		},
 		{
-			questionText: 'How many Harry Potter books are there?',
+			questionText: 'Qual das frases do Antoine de Saint-Exupéry (autor do Pequeno Príncipe) eu já escrevi num papelzinho e te dei?',
 			answerOptions: [
-				{ answerText: '1', isCorrect: false },
-				{ answerText: '4', isCorrect: false },
-				{ answerText: '6', isCorrect: false },
-				{ answerText: '7', isCorrect: true },
+				{ answerText: 'Tu te tornas eternamente responsável por aquilo que cativas.', isCorrect: false },
+				{ answerText: 'Amar não é olhar um para o outro, é olhar juntos na mesma direção.', isCorrect: false },
+				{ answerText: 'Só se vê bem com o coração, o essencial é invisível aos olhos.', isCorrect: false },
+				{ answerText: 'O verdadeiro amor nunca se desgasta. Quanto mais se dá mais se tem.', isCorrect: false },
+				{ answerText: 'Se alguém ama uma flor da qual só existe um exemplar em milhões e milhões de estrelas, isso basta para que seja feliz quando a contempla. Ele pensa: minha flor está lá, em algum lugar.', isCorrect: true },
+			],
+		},
+		{
+			questionText: 'Qual dos pratos abaixo ainda não cozinhamos para comermos juntas?',
+			answerOptions: [
+				{ answerText: 'Strogonoff tradicional', isCorrect: true },
+				{ answerText: 'Ovo', isCorrect: false },
+				{ answerText: 'Camarão com macarrão ao molho branco', isCorrect: false },
+				{ answerText: 'Risoto', isCorrect: false },
+				{ answerText: 'Frango grelhado', isCorrect: false },
+			],
+		},
+		{
+			questionText: 'Em que mês eu me apaixonei por você?',
+			answerOptions: [
+				{ answerText: 'Dezembro', isCorrect: false },
+				{ answerText: 'Fevereiro', isCorrect: false },
+				{ answerText: 'Março', isCorrect: true },
+				{ answerText: 'Abril', isCorrect: false },
+				{ answerText: 'Agosto', isCorrect: false },
+			],
+		},
+		{
+			questionText: 'Qual das informações abaixo nunca esteve no nome das playlist que fiz para você?',
+			answerOptions: [
+				{ answerText: '16', isCorrect: false },
+				{ answerText: 'Amor', isCorrect: true },
+				{ answerText: 'Achegar', isCorrect: false },
+				{ answerText: 'Poesia', isCorrect: false },
+				{ answerText: 'Exclamação', isCorrect: false },
+			],
+		},
+		{
+			questionText: 'Como é a minha frase marcante para você?',
+			answerOptions: [
+				{ answerText: 'Para. Pensa. Respira. Fala.', isCorrect: false },
+				{ answerText: 'Calma. Respira. Fala.', isCorrect: false },
+				{ answerText: 'Para. Respira. Pensa. Fala.', isCorrect: true },
+				{ answerText: 'Respira. Para. Pensa. Fala.', isCorrect: false },
+				{ answerText: 'Calma. Respira. Pensa. Fala.', isCorrect: false },
 			],
 		},
 	];
