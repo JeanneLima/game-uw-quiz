b60962ee7a2efbb87aefaca4245a9d8c3e0df920
Jeanne Lima <jeanne.lima@epimedsolutions.com>
2021-06-25 15:28:09 -0300

    Ajusta propriedade duplicada na home

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { FaRegHeart } from 'react-icons/fa'
+import { FaRegHeart } from 'react-icons/fa';
 
-import logoUW from '../../assets/images/logo_uw.png'
+import logoUW from '../../assets/images/logo_uw.png';
 
 import './styles.css';
 
@@ -13,21 +13,21 @@ const handleLoading = (e) => {
 	loading.classList.add('show');
 	bgLoading.classList.add('show');
 
-	setTimeout(function(){
+	setTimeout(function () {
 		loading.classList.remove('show');
 		bgLoading.classList.remove('show');
 	}, 800);
-}
+};
 
 function HomePage() {
 	return (
 		<div className='home-page'>
 			<div className="header-section">
 				<img alt="Logo UW" src={logoUW} className="bounce" />
-				<p>Breath. Look inside. Look up.<br/><strong>What do you see?</strong></p>
+				<p>Breath. Look inside. Look up.<br /><strong>What do you see?</strong></p>
 			</div>
 			<div className="buttons-section">
-				<Link to="/perguntas" onClick={(e) => handleLoading(e)}onClick={(e) => handleLoading(e)} className="btn btn-big btn-play" >Olhar com mais calma</Link>
+				<Link to="/perguntas" onClick={(e) => handleLoading(e)} className="btn btn-big btn-play" >Olhar com mais calma</Link>
 			</div>
 			<footer>
 				<span>Feito com <FaRegHeart /> por Jeanne Lima</span>
